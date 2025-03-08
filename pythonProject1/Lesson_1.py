d2cbeece67b93fe73087b8c7d74e9b8c2b2fed3f
vika998 <hlavatska.vika575@gmail.com>
2025-03-08 16:32:07 +0200

    Lesson 1. Creating a repository. Writing a function to find the minimum element of a list

---
+++ pythonProject1/Lesson_1.py
@@ -0,0 +1,10 @@
+def min(args):
+    min = args[0]
+    for num in args:
+        if min > num:
+            min = num
+    return min
+
+
+num_list = list(map(float, input().split()))
+print(min(num_list))
